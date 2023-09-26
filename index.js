aab9b8cd6c4173def68f9dc9bfa8bf304c13db7e
Anubhav Tripathi <anubhavtripathi0509@gmail.com>
2023-09-26 16:04:07 +0530

    commit

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-console.log("Nothing");
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+const userContainer = document.querySelector(".weather-container");
+
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-searchForm]");
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
+
+// Initially variables
+let currentTab = userTab;
 const API_KEY = '3b8d19b0856d441db3195219232609';
+currentTab.classList.add("current-tab")
+
+// ek kaam aur pending hai
 
 
 function renderWeatherInfo(data){
