d072b5fedc9fb2782cf14b3d816bddae38f16e3a
Anubhav Tripathi <anubhavtripathi0509@gmail.com>
2023-09-29 12:03:37 +0530

    commit

--- index.js
+++ index.js
@@ -81,10 +81,10 @@ async function renderWeatherInfo(weatherInfo) {
     countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`
     desc.innerText = weatherInfo?.weather?.[0]?.description;
     weatherIcon.src =`https://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`
-    temp.innerText = weatherInfo?.main?.temp;
-    windspeed.innerText = weatherInfo?.wind?.speed;
-    humidity.innerText = weatherInfo?.main?.humidity;
-    cloudiness.innerText = weatherInfo?.clouds?.all;
+    temp.innerText = `${weatherInfo?.main?.temp}°C`;
+    windspeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
+    humidity.innerText = `${weatherInfo?.main?.humidity}%`;
+    cloudiness.innerText = `${weatherInfo?.clouds?.all}%`;
     }
 
     catch(err){
